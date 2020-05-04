df4054a1f34164886720ed7eb8a7bdbbfdd47e22
Ishika Saini <ishikasaini75@gmail.com>
2020-05-04 16:22:10 +0530

    Changes in q3

--- Assignment4/src/q3/InterfaceExtender.java
+++ Assignment4/src/q3/InterfaceExtender.java
@@ -1,14 +1,28 @@
 package q3;
 
+import java.util.Scanner;
+
 import q3.InterfaceRunner.Num_Process;
 
 public class InterfaceExtender {
-	public static Num_Process Add = (a, b) -> a + b;
-	public static Num_Process Subtract = (a, b) -> a - b;
-	public static Num_Process Multiply = (a, b) -> a * b;
-	public static Num_Process Divide = (a, b) -> a / b;
+	public static void main(String[] args) {
+		
+		Num_Process[] ob = new Num_Process[4];
+		ob[0] = (a,b)->a+b;
+		ob[1] = (a,b)->a-b;
+		ob[2] = (a,b)->a*b;
+		ob[3] = (a,b)->a/b;
+		
+		Scanner scn = new Scanner(System.in);
 
-	public static int compute(int a, int b, Num_Process op) {
-		return op.cal(a, b);
+		System.out.println("Enter a & b: ");
+		int a = scn.nextInt();
+		int b = scn.nextInt();
+		
+		System.out.println("OPERATIONS\n1. Add\n2. Subtract\n3. Multiply\n4. Divide");
+		System.out.print("Enter choice: ");
+		int choice = scn.nextInt();
+		
+		System.out.println(ob[choice-1].cal(a, b));
 	}
 }
--- Assignment4/src/q3/InterfaceRunner.java
+++ Assignment4/src/q3/InterfaceRunner.java
@@ -1,28 +1,8 @@
 package q3;
 
-import java.util.Scanner;
-
 public class InterfaceRunner {
 
 	public interface Num_Process {
 		public int cal(int a, int b);
 	}
-
-	public static void main(String[] args)
-			throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
-		Scanner scn = new Scanner(System.in);
-
-		System.out.println("Enter a & b: ");
-		int a = scn.nextInt();
-		int b = scn.nextInt();
-		System.out.println("OPERATIONS\n1. Add\n2. Subtract\n3. Multiply\n4. Divide");
-		System.out.print("Enter choice: ");
-		String choice = scn.next();
-
-		InterfaceExtender ie = new InterfaceExtender();
-		Num_Process op = (Num_Process) InterfaceExtender.class.getField(choice).get(ie);
-		int ans = InterfaceExtender.compute(a, b, op);
-		System.out.println(ans);
-	}
-
 }
